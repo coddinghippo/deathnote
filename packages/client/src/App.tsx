f5a4194633fe9f5c14c306ee63cae080a4ef54fb
Kong Chae Won <chaewonkong@gmail.com>
2020-02-12 16:14:59 +0900

    style: add styled-components

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect } from "react";
-import logo from "./logo.svg";
-import "./App.css";
 import { api } from "./api";
+import { GlobalStyle } from "./styles/global-styles";
 
 const App = () => {
   useEffect(() => {
@@ -14,22 +13,10 @@ const App = () => {
   };
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyle />
+      <div className="App">Hello world</div>
+    </>
   );
 };
 

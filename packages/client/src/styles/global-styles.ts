36298840e85f83ea50267ecde23d3079d726ef97
Kong Chae Won <chaewonkong@gmail.com>
2020-02-12 16:15:14 +0900

    style: remove css files

---
+++ packages/client/src/styles/global-styles.ts
@@ -0,0 +1,38 @@
+import reset from "styled-reset";
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+${reset};
+  * {
+      box-sizing: border-box;
+      background: #000;
+      color: white;
+  }
+
+  body{
+    font-family: 'Nanum Gothic', sans-serif;
+  }
+
+  a{
+    color: unset;
+    &:hover{
+      color: unset;
+    }
+  }
+
+  strong{
+    font-weight: 700;
+  }
+  
+  button{
+    cursor: pointer;
+    background-color: white;
+    outline: none;
+    border: none;
+    &:active {
+      outline: none;
+      border: none;
+    }
+  }
+
+`;

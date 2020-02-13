d653eb631df67aa829f435d9cf84ea4bbe490deb
Kong Chae Won <chaewonkong@gmail.com>
2020-02-13 18:52:00 +0900

    feat: enable summoner search by name from input

--- packages/client/src/components/SearchView.tsx
+++ packages/client/src/components/SearchView.tsx
@@ -1,7 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
+import { api } from "../api";
+import { ISummoner } from "../shared-interfaces";
 
 const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const SearchWrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
@@ -25,18 +34,47 @@ const SearchButton = styled.div`
 `;
 
 const SearchView = () => {
+  const [summonerName, setSummonerName] = useState();
   const [summoner, setSummoner] = useState();
-  console.log(summoner);
 
-  const onChangeText = (text: string) => setSummoner(text);
+  // useEffect(() => {
+  //   getSummonerByName("상산조자룡이다");
+  // }, []);
+
+  const getSummonerByName = async (summonerName: string) => {
+    const data = await api.getSummonerByName(summonerName);
+    setSummoner(data);
+  };
+
+  const onChangeText = (text: string) => setSummonerName(text);
+
+  const onClickSearch = () => getSummonerByName(summonerName);
+
+  const renderSummonerData = (data: ISummoner) => {
+    return (
+      <div key={data.id}>
+        <h1>{data.name}</h1>
+        <p>{data.id}</p>
+        <p>
+          <b>Level: </b>
+          {data.summonerLevel}
+        </p>
+      </div>
+    );
+  };
 
   return (
     <Wrapper>
-      <InputBox
-        placeholder="Summoner to die"
-        onChange={e => onChangeText(e.target.value)}
-      ></InputBox>
-      <SearchButton>SEARCH</SearchButton>
+      <SearchWrapper>
+        <InputBox
+          placeholder="Summoner to die"
+          onChange={e => onChangeText(e.target.value)}
+        ></InputBox>
+        <SearchButton onClick={onClickSearch}>SEARCH</SearchButton>
+      </SearchWrapper>
+      {summoner && summoner.id ? (
+        <div>{renderSummonerData(summoner)}</div>
+      ) : null}
     </Wrapper>
   );
 };
